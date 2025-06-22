16282571df928c6c420da8ef036a73a822d6ece1
Waile <wailenaeem22@gmail.com>
2025-06-22 23:15:12 +0500

    Refactor App component to implement navigation and integrate React Query

--- App.tsx
+++ App.tsx
@@ -5,107 +5,91 @@
  * @format
  */
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import { QueryClient, QueryClientProvider, useQuery } from '@tanstack/react-query';
+import React, { useState } from 'react';
 import {
+  Button,
   ScrollView,
   StatusBar,
   StyleSheet,
-  Text,
   useColorScheme,
-  View,
+  View
 } from 'react-native';
 
 import {
   Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
+  Header
 } from 'react-native/Libraries/NewAppScreen';
+import Posts from './src/Posts';
+import PostById from './src/PostById';
+import CreatePost from './src/CreatePost';
+import { DefaultTheme, NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Home from './src/screens/Home';
+import Search from './src/screens/Search';
+import Analytics from './src/screens/Analytics';
+import Profile from './src/screens/Profile';
+import Wallet from './src/screens/Wallet';
+import CustomTab from './src/components/CustomTab';
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+
+const client = new QueryClient()
+
+const Tab = createBottomTabNavigator()
+
+
 
 function App(): React.JSX.Element {
+
   const isDarkMode = useColorScheme() === 'dark';
+  const [showPosts, setShowPosts] = useState(false)
 
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
-  /*
-   * To keep the template simple and small we're adding padding to prevent view
-   * from rendering under the System UI.
-   * For bigger apps the recommendation is to use `react-native-safe-area-context`:
-   * https://github.com/AppAndFlow/react-native-safe-area-context
-   *
-   * You can read more about it here:
-   * https://github.com/react-native-community/discussions-and-proposals/discussions/827
-   */
   const safePadding = '5%';
+      // CREATING A CUSTOM NAVIGATION THEME TO OVERRIDE THE DEFAULT BACKGROUND COLOR.
+    const navigationTheme = {
+        ...DefaultTheme,
+        colors: {
+            ...DefaultTheme.colors,
+            // SETTING THE BACKGROUND OF THE NAVIGATION CONTAINER.
+            background: "#ffffff",
+        },
+    };
 
   return (
-    <View style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        style={backgroundStyle}>
-        <View style={{paddingRight: safePadding}}>
-          <Header/>
-        </View>
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-            paddingHorizontal: safePadding,
-            paddingBottom: safePadding,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </View>
+    <NavigationContainer
+    theme={navigationTheme}
+    >
+    <QueryClientProvider
+    client={client}
+    >
+      {/* <Button
+      title='Toggle Posts'
+      onPress={() => setShowPosts(!showPosts)}
+      /> */}
+          {/* {showPosts && */}
+      {/* <CreatePost/> */}
+           {/* <Posts/>  */}
+           {/* }  */}
+          {/* <PostById
+          id={4}
+          />  */}
+          <Tab.Navigator
+          tabBar={(props)=> <CustomTab {...props} />}
+          >
+            <Tab.Screen name='Home' component={Home} />
+            <Tab.Screen name='Doctor' component={Search}/>
+            <Tab.Screen name='Lab' component={Analytics}/>
+            <Tab.Screen name='Pharmacy' component={Profile}/>
+            <Tab.Screen name='Appointment' component={Wallet}/>
+          </Tab.Navigator>
+
+    </QueryClientProvider>
+    </NavigationContainer>
   );
 }
 
